6ce119572be38e8110f102a3fd2bb459c18d76ed
PeddarmajiSaikumar <saikumarpeddarmaji@gmail.com>
2024-08-28 15:50:34 +0530

    Eureka Server and Clients added

--- emp_service/emp_service/src/main/java/com/ust/emp_service/feignclient/DepartmentClient.java
+++ emp_service/emp_service/src/main/java/com/ust/emp_service/feignclient/DepartmentClient.java
@@ -1,12 +1,8 @@
 package com.ust.emp_service.feignclient;
-
 import com.ust.emp_service.dto.DepartmentDto;
-import com.ust.emp_service.model.Employee;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 @FeignClient(name = "dept-service", url = "http://localhost:8080")
 public interface DepartmentClient {
